1c4defc9c7da3af513de2247014cf5355b3d63a8
Vladimir Gladkov <vovkos@gmail.com>
2014-08-11 17:31:01 +0800

    [ioninja] compiles and runs on linux-x86_64 -- finally

---
+++ include/axl_g_WarningSuppression.h
@@ -0,0 +1,29 @@
+// This file is part of AXL (R) Library
+// Tibbo Technology Inc (C) 2004-2014. All rights reserved
+// Author: Vladimir Gladkov
+
+//.............................................................................
+
+// no pragma once -- allow multiple inclusion of this file for re-enabling warning suppression
+
+// warning C4146: unary minus operator applied to unsigned type, result still unsigned
+// warning C4267: 'var' : conversion from 'size_t' to 'type', possible loss of data
+// warning C4355: 'this' : used in base member initializer list
+
+#if (_AXL_CPP == AXL_CPP_MSC)
+#	pragma warning (disable: 4146)
+#	pragma warning (disable: 4267)
+#	pragma warning (disable: 4355)
+#endif
+
+#if (_AXL_CPP == AXL_CPP_GCC)
+#	pragma GCC diagnostic ignored "-Wunused-parameter"
+#	pragma GCC diagnostic ignored "-Wparentheses"
+#	pragma GCC diagnostic ignored "-Wsign-compare"
+#	pragma GCC diagnostic ignored "-Wformat"
+#	pragma GCC diagnostic ignored "-Wmultichar"
+#	pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
+#	pragma GCC diagnostic ignored "-Winvalid-offsetof"
+#endif
+
+//.............................................................................
