dbd0dd6d1ca3a92635102b2f1ca70809bce2eb4e
Vladimir Gladkov <vovkos@gmail.com>
2015-08-18 14:28:09 +0300

    [axl] long-jmp-try can now return status via AXL_MT_END_LONG_JMP_TRY_EX (so AXL_MT_LONG_JMP_CATCH can be in many cases omitted)

--- include/axl_mt_LongJmpTry.h
+++ include/axl_mt_LongJmpTry.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#define _AXL_MT_TLSSLOT_H
+#define _AXL_MT_LONGJMPTRY_H
 
 #include "axl_mt_TlsSlot.h"
 
@@ -37,12 +37,20 @@ struct LongJmpTry
 			ASSERT (prev == &__axlLongJmpTry); \
 		}
 
-#define AXL_MT_END_LONG_JMP_TRY() \
+#define AXL_MT_END_LONG_JMP_TRY_IMPL() \
 	} \
 	{ \
 		axl::mt::LongJmpTry* prev = axl::mt::setTlsSlotValue <axl::mt::LongJmpTry> (__axlPrevLongJmpTry); \
 		ASSERT (prev == &__axlLongJmpTry || prev == __axlPrevLongJmpTry); \
 	} \
+
+#define AXL_MT_END_LONG_JMP_TRY() \
+	AXL_MT_END_LONG_JMP_TRY_IMPL() \
+}
+
+#define AXL_MT_END_LONG_JMP_TRY_EX(result) \
+	AXL_MT_END_LONG_JMP_TRY_IMPL() \
+	*(result) = branch == 0; \
 }
 
 #define AXL_MT_LONG_JMP_THROW() \
