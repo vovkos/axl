a335805ad4fa19fc163ae2198723fd743545e5bf
Vladimir Gladkov <vovkos@gmail.com>
2015-10-02 18:34:51 +0800

    [axl] rtl::StringSlice

---
+++ include/axl_rtl_ArraySlice.h
@@ -0,0 +1,19 @@
+// This file is part of AXL (R) Library
+// Tibbo Technology Inc (C) 2004-2014. All rights reserved
+// Author: Vladimir Gladkov
+
+#pragma once
+
+#define _AXL_RTL_ARRAYSLICE_H
+
+#include "axl_rtl_Array.h"
+
+namespace axl {
+namespace rtl {
+
+//.............................................................................
+
+//.............................................................................
+
+} // namespace rtl
+} // namespace axl
