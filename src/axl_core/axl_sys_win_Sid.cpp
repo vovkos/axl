dd8821f5e40bf5726fd9663119ff33045b2776ed
Vladimir Gladkov <vovkos@gmail.com>
2016-04-29 11:14:12 +0800

    [axl] sys::win::Sid major improvements (name lookups, sid lookups, parsing etc)

--- src/axl_core/axl_sys_win_Sid.cpp
+++ src/axl_core/axl_sys_win_Sid.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "axl_sys_win_Sid.h"
+#include "axl_sys_win_Handle.h"
+#include "axl_err_Error.h"
 
 namespace axl {
 namespace sys {
@@ -8,38 +10,169 @@ namespace win {
 //.............................................................................
 
 bool 
-Sid::create (
+Sid::create_va (
 	SID_IDENTIFIER_AUTHORITY* identifierAuthority,
 	size_t subAuthorityCount,
-	dword_t subAuthority0,
-	dword_t subAuthority1,
-	dword_t subAuthority2,
-	dword_t subAuthority3,
-	dword_t subAuthority4,
-	dword_t subAuthority5,
-	dword_t subAuthority6,
-	dword_t subAuthority7
+	axl_va_list va
 	)
 {
-	close ();
+	size_t size = ::GetSidLengthRequired ((uchar_t) subAuthorityCount);
+	SID* sid = getBuffer (size, false);
+	if (!sid)
+		return false;
 
-	bool_t result = ::AllocateAndInitializeSid (
-		identifierAuthority,
-		subAuthorityCount,
-		subAuthority0,
-		subAuthority1,
-		subAuthority2,
-		subAuthority3,
-		subAuthority4,
-		subAuthority5,
-		subAuthority6,
-		subAuthority7,
-		&m_h
-		);
+	bool_t result = ::InitializeSid (sid, identifierAuthority, (uchar_t) subAuthorityCount);
+	if (!result)
+		return err::failWithLastSystemError ();
 
-	return err::complete (result);
+	dword_t* subAuthorityTable = ::GetSidSubAuthority (sid, 0);
+
+	for (size_t i = 0; i < subAuthorityCount; i++)
+		subAuthorityTable [i] = AXL_VA_ARG (va, dword_t);
+
+	return true;
 }
 
+bool 
+Sid::parse (const char* string)
+{
+	bool_t result;
+	SID* sid;
+
+	result = ::ConvertStringSidToSidA (string, (PSID*) &sid);
+	if (!result)
+		return err::failWithLastSystemError ();
+
+	result = copy (sid);
+	::LocalFree (sid);	
+	return result != 0;
+}
+
+bool 
+Sid::parse (const wchar_t* string)
+{
+	bool_t result;
+	SID* sid;
+
+	result = ::ConvertStringSidToSidW (string, (PSID*) &sid);
+	if (!result)
+		return err::failWithLastSystemError ();
+
+	result = copy (sid);
+	::LocalFree (sid);	
+	return result != 0;
+}
+
+bool
+Sid::getString (sl::String* string) const
+{
+	if (!m_p)
+	{
+		string->clear ();
+		return true;
+	}
+
+	char* stringBuffer;
+	bool_t result = ::ConvertSidToStringSidA (m_p, &stringBuffer);
+	if (!result)
+		return err::failWithLastSystemError ();
+
+	result = string->copy (stringBuffer);
+	::LocalFree(stringBuffer);
+	return result != 0;
+}
+
+bool 
+Sid::lookupAccountName (
+	const char* systemName,
+	const char* accountName,
+	SID_NAME_USE* sidType
+	)
+{
+	char sidBuffer [SECURITY_MAX_SID_SIZE];
+	dword_t sidSize = sizeof (sidBuffer);
+	char domainNameBuffer [MAX_PATH];
+	dword_t domainNameLength = sizeof (domainNameBuffer);
+	SID_NAME_USE dummySidType;
+
+	bool_t result = ::LookupAccountNameA (
+		systemName, 
+		accountName, 
+		(SID*) sidBuffer, 
+		&sidSize, 
+		domainNameBuffer, 
+		&domainNameLength, 
+		sidType ? sidType : &dummySidType
+		);
+
+	if (!result)	
+		return err::failWithLastSystemError ();
+
+	return copy ((SID*) sidBuffer);
+}
+
+bool 
+Sid::lookupAccountName (
+	const wchar_t* systemName,
+	const wchar_t* accountName,
+	SID_NAME_USE* sidType
+	)
+{
+	char sidBuffer [SECURITY_MAX_SID_SIZE];
+	dword_t sidSize = sizeof (sidBuffer);
+	wchar_t domainNameBuffer [MAX_PATH];
+	dword_t domainNameLength = sizeof (domainNameBuffer) / sizeof (wchar_t);
+	SID_NAME_USE dummySidType;
+
+	bool_t result = ::LookupAccountNameW (
+		systemName, 
+		accountName, 
+		(SID*) sidBuffer, 
+		&sidSize, 
+		domainNameBuffer, 
+		&domainNameLength, 
+		sidType ? sidType : &dummySidType
+		);
+
+	if (!result)	
+		return err::failWithLastSystemError ();
+
+	return copy ((SID*) sidBuffer);
+}
+
+bool 
+Sid::lookupAccountSid (
+	const char* systemName,
+	const SID* sid,
+	sl::String* accountName,
+	SID_NAME_USE* sidType
+	)
+{
+	char accountNameBuffer [MAX_PATH];
+	dword_t accountNameLength = sizeof (accountNameBuffer);
+	char domainNameBuffer [MAX_PATH];
+	dword_t domainNameLength = sizeof (domainNameBuffer);
+	SID_NAME_USE dummySidType;
+
+	if (!sid)
+		return err::fail (err::SystemErrorCode_InvalidParameter);
+
+	bool_t result = ::LookupAccountSidA (
+		systemName, 
+		(SID*) sid, 
+		accountNameBuffer, 
+		&accountNameLength, 
+		domainNameBuffer, 
+		&domainNameLength, 
+		sidType ? sidType : &dummySidType
+		);
+
+	if (!result)	
+		return err::failWithLastSystemError ();
+
+	accountName->copy (accountNameBuffer, accountNameLength);
+	return true;
+}
 //.............................................................................
 
 } // namespace win
--- src/axl_core/pch.h
+++ src/axl_core/pch.h
@@ -2,7 +2,9 @@
 
 #include "axl_g_Pch.h"
 
-#if (_AXL_POSIX == AXL_POSIX_LINUX && defined _AXL_SYS_LNX_LIBUDEV)
+#if (_AXL_ENV == AXL_ENV_WIN)
+#	include <sddl.h>
+#elif (_AXL_POSIX == AXL_POSIX_LINUX && defined _AXL_SYS_LNX_LIBUDEV)
 #	include <libudev.h>
 #elif (_AXL_POSIX == AXL_POSIX_DARWIN)
 #	include <CoreFoundation/CoreFoundation.h>
