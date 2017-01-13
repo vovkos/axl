3046babbca7d4499bf97eb5bf3490d1f961c93c8
Vladimir Gladkov <vovkos@gmail.com>
2017-01-13 18:20:16 +0800

    [travis] added osx build

---
+++ travis/install-osx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+brew update
+brew install lua
+brew install libusb
+brew install ragel
+
+# openssl is already installed, but not linked
+
+echo "set (OPENSSL_INC_DIR /usr/local/opt/openssl/include)" >> paths.cmake
+echo "set (OPENSSL_LIB_DIR /usr/local/opt/openssl/lib)" >> paths.cmake
