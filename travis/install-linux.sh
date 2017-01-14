2e26b8c6a22b16403a6ed221a777d77f203ebbeb
Vladimir Gladkov <vovkos@gmail.com>
2017-01-14 23:38:23 +0800

    [travis] added x86/amd64 x Debug/Release to build matrix

--- travis/install-linux.sh
+++ travis/install-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LUA_VERSION=5.2
+# manually install CMake -- we need at least CMake 3.3
 
 CMAKE_VERSION=3.3.2
 CMAKE_VERSION_DIR=v3.3
@@ -14,8 +14,39 @@ mkdir -p $CMAKE_DIR
 tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
 export PATH=$CMAKE_DIR/bin:$PATH
 
-sudo apt-get -qq update
-sudo apt-get install -y liblua${LUA_VERSION}-dev
-sudo apt-get install -y libpcap-dev
-sudo apt-get install -y libudev-dev
-sudo apt-get install -y libusb-1.0
+# now to official APT packages
+
+if [ "$TARGET_CPU" != "x86" ]; then
+	sudo apt-get -qq update
+
+	sudo apt-get install -y liblua5.2-dev
+	sudo apt-get install -y libpcap-dev
+	sudo apt-get install -y libudev-dev
+	sudo apt-get install -y libusb-1.0
+else
+	sudo dpkg --add-architecture i386
+	sudo apt-get -qq update
+
+	sudo apt-get install -y liblua5.2-dev:i386
+	sudo apt-get install -y libpcap0.8-dev:i386
+	sudo apt-get install -y libudev-dev:i386
+	sudo apt-get install -y libusb-1.0:i386
+
+	# OpenSSL and Expat are already installed, but 64-bit only
+
+	sudo apt-get install -y libssl-dev:i386
+	sudo apt-get install -y libexpat-dev:i386
+
+	# install g++-multilib -- in the end, after i386 packages!
+
+	sudo apt-get install -y g++-multilib
+
+	# CMake fails to properly switch between 32-bit and 64-bit libraries on Ubuntu
+
+	echo "set (OPENSSL_LIB_DIR /usr/lib/i386-linux-gnu)" >> paths.cmake
+	echo "set (EXPAT_LIB_DIR /usr/lib/i386-linux-gnu)" >> paths.cmake
+	echo "set (LUA_LIB_DIR /usr/lib/i386-linux-gnu)" >> paths.cmake
+	echo "set (PCAP_LIB_DIR /usr/lib/i386-linux-gnu)" >> paths.cmake
+	echo "set (LIBUSB_LIB_DIR /usr/lib/i386-linux-gnu)" >> paths.cmake
+fi
+
