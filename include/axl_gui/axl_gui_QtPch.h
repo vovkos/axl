c7280499b62ce8783ec65ddbc145e11ae4a9ac55
Vladimir Gladkov <vovkos@gmail.com>
2018-03-14 09:06:17 +0800

    [axl_gui] added QT_NO_VERSION_TAGGING
    by default, QT headers force linkage to the qt_version_tag symbol
    this makes it impossible to use axl::gui members without linking to QT
    -- even if QT is really not required!

--- include/axl_gui/axl_gui_QtPch.h
+++ include/axl_gui/axl_gui_QtPch.h
@@ -15,6 +15,12 @@
 
 #include "axl_sl_StringHashTable.h"
 
+// by default, QT headers force linkage to the qt_version_tag symbol
+// this makes it impossible to use axl::gui members without linking to QT --
+// even if QT is really not required!
+
+#define QT_NO_VERSION_TAGGING 1
+
 #include <QAbstractScrollArea>
 #include <QApplication>
 #include <QClipboard>
