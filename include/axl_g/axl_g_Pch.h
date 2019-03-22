88c7f65179a49dde3f0400a678c5c58832ba78dd
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2019-03-22 15:56:02 +0800

    [axl_g] ignore MSVC warning 4091 (pops up on msvc15 with older platform headers)

--- include/axl_g/axl_g_Pch.h
+++ include/axl_g/axl_g_Pch.h
@@ -654,6 +654,7 @@ axl_trace(
 //..............................................................................
 
 #if (_AXL_CPP_MSC)
+#	pragma warning(disable: 4091) // warning C4091: 'typedef ': ignored on left of '...' when no variable is declared
 #	pragma warning(disable: 4146) // warning C4146: unary minus operator applied to unsigned type, result still unsigned
 #	pragma warning(disable: 4267) // warning C4267: 'var' : conversion from 'size_t' to 'type', possible loss of data
 #	pragma warning(disable: 4355) // warning C4355: 'this' : used in base member initializer list
