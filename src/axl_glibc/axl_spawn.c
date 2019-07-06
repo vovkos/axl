71564a4195403dadd58a5eaffd33a8e56dac622e
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2019-07-06 23:45:06 +0800

    [axl_glibc] added license header

--- src/axl_glibc/axl_spawn.c
+++ src/axl_glibc/axl_spawn.c
@@ -1,3 +1,14 @@
+//..............................................................................
+//
+//  This file is part of the AXL library.
+//
+//  AXL is distributed under the MIT license.
+//  For details see accompanying license.txt file,
+//  the public copy of which is also available at:
+//  http://tibbo.com/downloads/archive/axl/license.txt
+//
+//..............................................................................
+
 #include <spawn.h>
 
 __asm__(".symver posix_spawn,  posix_spawn@GLIBC_2.2.5");
