440367465ef34b4557b4253b7e5c5527643a95fb
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2020-06-11 16:15:03 +0800

    [axl_test_con] receipt verification test

--- test/axl_test_con/main.cpp
+++ test/axl_test_con/main.cpp
@@ -20,6 +20,8 @@
 #include "axl_spy_ModuleEnumerator.h"
 #include "axl_spy_Hook.h"
 
+#include "axl_cry_AppStoreReceipt.h"
+
 //..............................................................................
 
 #if (_AXL_OS_WIN)
@@ -4272,19 +4274,22 @@ printSslCertificate(X509* cert)
 }
 
 void
-copyPrintSslCertificate(X509* cert)
+copyPrintSslCertificate(X509* cert0)
 {
-	sl::Array<char> buffer = cry::saveX509_der(cert);
-	X509* copyCert = cry::loadX509_der(buffer);
+	cry::X509Cert cert = cert0;
+	cry::X509Cert copyCert;
+
+	sl::Array<char> buffer = cert.saveDer();
+	copyCert.loadDer(buffer, buffer.getCount());
 	printf("DER-copy:\n");
 	printSslCertificate(copyCert);
-	X509_free(copyCert);
 
-	sl::String string = cry::saveX509_pem(cert);
-	copyCert = cry::loadX509_pem(string);
+	sl::String string = cert.savePem();
+	copyCert.loadPem(string);
 	printf("PEM-copy:\n");
 	printSslCertificate(copyCert);
-	X509_free(copyCert);
+
+	cert.detach();
 }
 
 int
@@ -5226,303 +5231,6 @@ spyStdcallTest()
 
 //..............................................................................
 
-struct AppReceipt
-{
-	sl::String m_bundleId;
-	sl::String m_appVersion;
-	sl::String m_originalAppVersion;
-	sl::String m_receiptCreationDate;
-	sl::String m_receiptExpirationDate;
-	sl::Array<char> m_opaque;
-	sl::Array<char> m_sha1Hash;
-
-	void
-	clear();
-};
-
-// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-
-void
-AppReceipt::clear()
-{
-	m_bundleId.clear();
-	m_appVersion.clear();
-	m_originalAppVersion.clear();
-	m_opaque.clear();
-	m_sha1Hash.clear();
-}
-
-//..............................................................................
-
-class AppReceiptParser
-{
-protected:
-	enum State
-	{
-		State_Idle = 0,
-		State_Receipt,
-		State_Attribute,
-		State_AttributeType,
-		State_AttributeVersion,
-		State_AttributeValue,
-		State_AttributeStringValue,
-	};
-
-	enum AttributeId
-	{
-		AttributeId_Undefined             = 0,
-		AttributeId_BundleId              = 2,
-		AttributeId_AppVersion            = 3,
-		AttributeId_Opaque                = 4,
-		AttributeId_Sha1Hash              = 5,
-		AttributeId_ReceiptCreationDate   = 12,
-		AttributeId_OriginalAppVersion    = 19,
-		AttributeId_ReceiptExpirationDate = 21,
-	};
-
-	AppReceipt* m_receipt;
-	AttributeId m_attributeId;
-	sl::String* m_attributeString;
-
-public:
-	AppReceiptParser();
-
-	bool
-	parse(
-		AppReceipt* receipt,
-		const void* p,
-		size_t size
-		);
-
-protected:
-	const char*
-	decode(
-		State state,
-		int expectedTag,
-		const char* unexpectedTagError,
-		const char* p,
-		size_t size
-		);
-};
-
-// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-
-AppReceiptParser::AppReceiptParser()
-{
-	m_receipt = NULL;
-	m_attributeId = AttributeId_Undefined;
-	m_attributeString = NULL;
-}
-
-bool
-AppReceiptParser::parse(
-	AppReceipt* receipt,
-	const void* p,
-	size_t size
-	)
-{
-	m_receipt = receipt;
-	m_attributeId = AttributeId_Undefined;
-	m_attributeString = NULL;
-
-	return decode(
-		State_Receipt,
-		V_ASN1_SET,
-		"invalid app receipt (expected ASN1 SET)",
-		(char*)p,
-		size
-		) != NULL;
-}
-
-const char*
-AppReceiptParser::decode(
-	State state,
-	int expectedTag,
-	const char* unexpectedTagError,
-	const char* p,
-	size_t size
-	)
-{
-	long length;
-	int tag;
-	int cls;
-
-	int retCode = ASN1_get_object((const uchar_t**)&p, &length, &tag, &cls, size);
-	if (retCode & 0x80)
-	{
-		cry::setLastCryptoError();
-		return NULL;
-	}
-
-	if (tag != expectedTag)
-	{
-		err::setError(unexpectedTagError);
-		return NULL;
-	}
-
-	const char* end = p + length;
-
-	switch (state)
-	{
-	case State_Receipt:
-		while (p < end)
-		{
-			p = decode(
-				State_Attribute,
-				V_ASN1_SEQUENCE,
-				"invalid app receipt attribute (expected ASN1 SEQUENCE)",
-				p,
-				end - p
-				);
-
-			if (!p)
-				return NULL;
-		}
-
-		break;
-
-	case State_Attribute:
-		m_attributeId = AttributeId_Undefined;
-		m_attributeString = NULL;
-
-		p = decode(
-			State_AttributeType,
-			V_ASN1_INTEGER,
-			"invalid app receipt attribute type (expected ASN1 INTEGER)",
-			p,
-			end - p
-			);
-
-		if (!p)
-			return NULL;
-
-		p = decode(
-			State_AttributeVersion,
-			V_ASN1_INTEGER,
-			"invalid app receipt attribute version (expected ASN1 INTEGER)",
-			p,
-			end - p
-			);
-
-		if (!p)
-			return NULL;
-
-		p = decode(
-			State_AttributeValue,
-			V_ASN1_OCTET_STRING,
-			"invalid app receipt attribute value (expected ASN1 OCTET STRING)",
-			p,
-			end - p
-			);
-
-		if (!p)
-			return NULL;
-
-		break;
-
-	case State_AttributeType:
-		memcpy(&m_attributeId, p, AXL_MIN(length, sizeof(m_attributeId)));
-		break;
-
-	case State_AttributeVersion:
-		break; // ignore
-
-	case State_AttributeValue:
-		switch (m_attributeId)
-		{
-		case AttributeId_BundleId:
-			m_attributeString = &m_receipt->m_bundleId;
-
-			p = decode(
-				State_AttributeStringValue,
-				V_ASN1_UTF8STRING,
-				"invalid app receipt bundle ID (expected ASN1 UTF8 STRING)",
-				p,
-				length
-				);
-
-			break;
-
-		case AttributeId_AppVersion:
-			m_attributeString = &m_receipt->m_appVersion;
-
-			p = decode(
-				State_AttributeStringValue,
-				V_ASN1_UTF8STRING,
-				"invalid app receipt version (expected ASN1 UTF8 STRING)",
-				p,
-				length
-				);
-
-			break;
-
-		case AttributeId_ReceiptCreationDate:
-			m_attributeString = &m_receipt->m_receiptCreationDate;
-
-			p = decode(
-				State_AttributeStringValue,
-				V_ASN1_IA5STRING,
-				"invalid app receipt creation date (expected ASN1 IA5 STRING)",
-				p,
-				length
-				);
-
-			break;
-
-		case AttributeId_OriginalAppVersion:
-			m_attributeString = &m_receipt->m_originalAppVersion;
-
-			p = decode(
-				State_AttributeStringValue,
-				V_ASN1_UTF8STRING,
-				"invalid app receipt original version (expected ASN1 UTF8 STRING)",
-				p,
-				length
-				);
-
-			break;
-
-		case AttributeId_ReceiptExpirationDate:
-			m_attributeString = &m_receipt->m_receiptExpirationDate;
-
-			p = decode(
-				State_AttributeStringValue,
-				V_ASN1_IA5STRING,
-				"invalid app receipt expiration date (expected ASN1 IA5 STRING)",
-				p,
-				length
-				);
-
-			break;
-
-		case AttributeId_Opaque:
-			m_receipt->m_opaque.copy(p, length);
-			break;
-
-		case AttributeId_Sha1Hash:
-			m_receipt->m_sha1Hash.copy(p, length);
-			break;
-		}
-
-		if (!p)
-			return NULL;
-
-		break;
-
-	case State_AttributeStringValue:
-		ASSERT(m_attributeString);
-		m_attributeString->copy(p, length);
-		break;
-
-	default:
-		ASSERT(false);
-	}
-
-	return end;
-}
-
-//..............................................................................
-
 int
 receiptTest()
 {
@@ -5548,67 +5256,17 @@ receiptTest()
 
 	printf("file size: %d\n", file.getFileSize());
 
-	cry::Pkcs7Handle p7 = cry::loadPkcs7_der(file.p(), file.getMappingSize());
-	if (!p7)
-	{
-		printf("PKCS7 load error: %s\n", err::getLastErrorDescription().sz());
-		return -1;
-	}
+#if (_AXL_OS_DARWIN)
+	uint_t flags = 0;
+#else
+	uint_t flags = cry::VerifyAppReceiptFlag_SkipHashCheck;
+#endif
 
-	printf("Receipt type: %s\n", cry::getAsn1ObjectString(p7->type).sz());
-
-	static const char appleRootCertPem[] =
-		"-----BEGIN CERTIFICATE-----\n"
-		"MIIEuzCCA6OgAwIBAgIBAjANBgkqhkiG9w0BAQUFADBiMQswCQYDVQQGEwJVUzET\n"
-		"MBEGA1UEChMKQXBwbGUgSW5jLjEmMCQGA1UECxMdQXBwbGUgQ2VydGlmaWNhdGlv\n"
-		"biBBdXRob3JpdHkxFjAUBgNVBAMTDUFwcGxlIFJvb3QgQ0EwHhcNMDYwNDI1MjE0\n"
-		"MDM2WhcNMzUwMjA5MjE0MDM2WjBiMQswCQYDVQQGEwJVUzETMBEGA1UEChMKQXBw\n"
-		"bGUgSW5jLjEmMCQGA1UECxMdQXBwbGUgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkx\n"
-		"FjAUBgNVBAMTDUFwcGxlIFJvb3QgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAw\n"
-		"ggEKAoIBAQDkkakJH5HbHkdQ6wXtXnmELes2oldMVeyLGYne+Uts9QerIjAC6Bg+\n"
-		"+FAJ039BqJj50cpmnCRrEdCju+QbKsMflZ56DKRHi1vUFjczy8QPTc4UadHJGXL1\n"
-		"XQ7Vf1+b8iUDulWPTV0N8WQ1IxVLFVkds5T39pyez1C6wVhQZ48ItCD3y6wsIG9w\n"
-		"tj8BMIy3Q88PnT3zK0koGsj+zrW5DtleHNbLPbU6rfQPDgCSC7EhFi501TwN22IW\n"
-		"q6NxkkdTVcGvL0Gz+PvjcM3mo0xFfh9Ma1CWQYnEdGILEINBhzOKgbEwWOxaBDKM\n"
-		"aLOPHd5lc/9nXmW8Sdh2nzMUZaF3lMktAgMBAAGjggF6MIIBdjAOBgNVHQ8BAf8E\n"
-		"BAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUK9BpR5R2Cf70a40uQKb3\n"
-		"R01/CF4wHwYDVR0jBBgwFoAUK9BpR5R2Cf70a40uQKb3R01/CF4wggERBgNVHSAE\n"
-		"ggEIMIIBBDCCAQAGCSqGSIb3Y2QFATCB8jAqBggrBgEFBQcCARYeaHR0cHM6Ly93\n"
-		"d3cuYXBwbGUuY29tL2FwcGxlY2EvMIHDBggrBgEFBQcCAjCBthqBs1JlbGlhbmNl\n"
-		"IG9uIHRoaXMgY2VydGlmaWNhdGUgYnkgYW55IHBhcnR5IGFzc3VtZXMgYWNjZXB0\n"
-		"YW5jZSBvZiB0aGUgdGhlbiBhcHBsaWNhYmxlIHN0YW5kYXJkIHRlcm1zIGFuZCBj\n"
-		"b25kaXRpb25zIG9mIHVzZSwgY2VydGlmaWNhdGUgcG9saWN5IGFuZCBjZXJ0aWZp\n"
-		"Y2F0aW9uIHByYWN0aWNlIHN0YXRlbWVudHMuMA0GCSqGSIb3DQEBBQUAA4IBAQBc\n"
-		"NplMLXi37Yyb3PN3m/J20ncwT8EfhYOFG5k9RzfyqZtAjizUsZAS2L70c5vu0mQP\n"
-		"y3lPNNiiPvl4/2vIB+x9OYOLUyDTOMSxv5pPCmv/K/xZpwUJfBdAVhEedNO3iyM7\n"
-		"R6PVbyTi69G3cN8PReEnyvFteO3ntRcXqNx+IjXKJdXZD9Zr1KIkIxH3oayPc4Fg\n"
-		"xhtbCS+SsvhESPBgOJ4V9T0mZyCKM2r3DYLP3uujL/lTaltkwGMzd/c6ByxW69oP\n"
-		"IQ7aunMZT7XZNn/Bh1XZp5m5MkL72NVxnn6hUrcbvZNCJBIqxw8dtk2cXmPIS4AX\n"
-		"UKqK1drk/NAJBzewdXUh\n"
-		"-----END CERTIFICATE-----\n";
-
-	cry::X509Handle appleRootCert = cry::loadX509_pem(appleRootCertPem);
-	cry::X509StoreHandle store = X509_STORE_new();
-	X509_STORE_add_cert(store, appleRootCert);
-
-	cry::Bio bio;
-	bio.createMem();
-	retCode = PKCS7_verify(p7, NULL, store, NULL, bio, 0);
-	if (!retCode)
-	{
-		cry::setLastCryptoError();
-		printf("verify error: %s\n", err::getLastErrorDescription().sz());
-		return -1;
-	}
-
-	BUF_MEM* bufMem = bio.getBufMem();
-
-	AppReceipt receipt;
-	AppReceiptParser parser;
-	result = parser.parse(&receipt, bufMem->data, bufMem->length);
+	cry::AppStoreReceipt receipt;
+	result = cry::verifyAppStoreReceipt(&receipt, file.p(), file.getMappingSize(), flags);
 	if (!result)
 	{
-		printf("parse error: %s\n", err::getLastErrorDescription().sz());
+		printf("verify error: %s\n", err::getLastErrorDescription().sz());
 		return -1;
 	}
 
