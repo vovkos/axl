56c2aa847e52d2e2fe6ab6f97dc3e00b09a4f1a8
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2021-07-13 23:56:54 +0800

    [ci] try to use chocolatey

--- ci/github-actions/install-windows.bat
+++ ci/github-actions/install-windows.bat
@@ -18,52 +18,65 @@ set DOWNLOAD_DIR_CMAKE=%DOWNLOAD_DIR:\=/%
 
 :: Ragel
 
-echo Downloading Ragel...
+:: echo Downloading Ragel...
+::
+:: mkdir %DOWNLOAD_DIR%
+:: powershell "Invoke-WebRequest -Uri %RAGEL_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\ragel.exe"
+::
+:: echo set (RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
 
-mkdir %DOWNLOAD_DIR%
-powershell "Invoke-WebRequest -Uri %RAGEL_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\ragel.exe"
+echo Installing Ragel...
 
-echo set (RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
+choco install ragel
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
 :: Lua (download from SourceForge, need PowerShell user agent to handle redirect)
 
-echo Downloading Lua...
+echo Installing Lua...
 
-mkdir %DOWNLOAD_DIR%\lua
-powershell "Invoke-WebRequest -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -Uri %LUA_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\lua\lua.zip"
-7z x -y %DOWNLOAD_DIR%\lua\lua.zip -o%DOWNLOAD_DIR%\lua
+choco install lua53
 
-echo set (LUA_INC_DIR %DOWNLOAD_DIR_CMAKE%/lua/include) >> paths.cmake
-echo set (LUA_LIB_DIR %DOWNLOAD_DIR_CMAKE%/lua) >> paths.cmake
-echo set (LUA_DLL_DIR %DOWNLOAD_DIR_CMAKE%/lua) >> paths.cmake
-echo set (LUA_LIB_NAME %LUA_LIB_NAME%) >> paths.cmake
+:: echo Downloading Lua...
+::
+:: mkdir %DOWNLOAD_DIR%\lua
+:: powershell "Invoke-WebRequest -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -Uri %LUA_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\lua\lua.zip"
+:: 7z x -y %DOWNLOAD_DIR%\lua\lua.zip -o%DOWNLOAD_DIR%\lua
+::
+:: echo set (LUA_INC_DIR %DOWNLOAD_DIR_CMAKE%/lua/include) >> paths.cmake
+:: echo set (LUA_LIB_DIR %DOWNLOAD_DIR_CMAKE%/lua) >> paths.cmake
+:: echo set (LUA_DLL_DIR %DOWNLOAD_DIR_CMAKE%/lua) >> paths.cmake
+:: echo set (LUA_LIB_NAME %LUA_LIB_NAME%) >> paths.cmake
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
 :: OpenSSL
 
-echo Downloading OpenSSL...
-
-powershell "Invoke-WebRequest -Uri %OPENSSL_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\openssl.msi"
-
 echo Installing OpenSSL...
 
-set OPENSSL_DIR=C:\Program Files\OpenSSL-%OPENSSL_PLATFORM%
+choco install lua52
 
-start /wait msiexec /i %DOWNLOAD_DIR%\openssl.msi INSTALLDIR=%OPENSSL_DIR% TARGETDIR=%OPENSSL_DIR% /qn /L*V %DOWNLOAD_DIR%\openssl-install-log.txt
-
-type %DOWNLOAD_DIR%\openssl-install-log.txt
-
-dir %OPENSSL_DIR%
-
-echo set (OPENSSL_INC_DIR %OPENSSL_ROOT_DIR%/%OPENSSL_PLATFORM%/include) >> paths.cmake
-echo set (OPENSSL_LIB_DIR %OPENSSL_ROOT_DIR%/%OPENSSL_PLATFORM%/lib) >> paths.cmake
-echo set (OPENSSL_DLL_DIR %OPENSSL_ROOT_DIR%/%OPENSSL_PLATFORM%/bin) >> paths.cmake
-echo set (OPENSSL_CRYPTO_LIB_NAME libcrypto) >> paths.cmake
-echo set (OPENSSL_CRYPTO_DLL_NAME libcrypto%OPENSSL_DLL_SUFFIX%) >> paths.cmake
-echo set (OPENSSL_SSL_LIB_NAME libssl) >> paths.cmake
-echo set (OPENSSL_SSL_DLL_NAME libssl%OPENSSL_DLL_SUFFIX%) >> paths.cmake
+:: %DOWNLOAD_DIR%\openssl-install-log.txt
+:: echo Downloading OpenSSL...
+::
+:: powershell "Invoke-WebRequest -Uri %OPENSSL_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\openssl.msi"
+::
+:: echo Installing OpenSSL...
+::
+:: set OPENSSL_DIR=C:\Program Files\OpenSSL-%OPENSSL_PLATFORM%
+::
+:: start /wait msiexec /i %DOWNLOAD_DIR%\openssl.msi INSTALLDIR=%OPENSSL_DIR% TARGETDIR=%OPENSSL_DIR% /qn /L*V %DOWNLOAD_DIR%\openssl-install-log.txt
+::
+:: type %DOWNLOAD_DIR%\openssl-install-log.txt
+::
+:: dir %OPENSSL_DIR%
+::
+:: echo set (OPENSSL_INC_DIR %OPENSSL_ROOT_DIR%/%OPENSSL_PLATFORM%/include) >> paths.cmake
+:: echo set (OPENSSL_LIB_DIR %OPENSSL_ROOT_DIR%/%OPENSSL_PLATFORM%/lib) >> paths.cmake
+:: echo set (OPENSSL_DLL_DIR %OPENSSL_ROOT_DIR%/%OPENSSL_PLATFORM%/bin) >> paths.cmake
+:: echo set (OPENSSL_CRYPTO_LIB_NAME libcrypto) >> paths.cmake
+:: echo set (OPENSSL_CRYPTO_DLL_NAME libcrypto%OPENSSL_DLL_SUFFIX%) >> paths.cmake
+:: echo set (OPENSSL_SSL_LIB_NAME libssl) >> paths.cmake
+:: echo set (OPENSSL_SSL_DLL_NAME libssl%OPENSSL_DLL_SUFFIX%) >> paths.cmake
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
