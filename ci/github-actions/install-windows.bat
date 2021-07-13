59f67f8d67a5a004ba237bfeebf7e0d1d08a79f5
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2021-07-13 11:59:57 +0800

    [ci] powershell typo fix

--- ci/github-actions/install-windows.bat
+++ ci/github-actions/install-windows.bat
@@ -19,7 +19,7 @@ set DOWNLOAD_DIR_CMAKE=%DOWNLOAD_DIR:\=/%
 :: Ragel
 
 mkdir %DOWNLOAD_DIR%\ragel
-powerhell "Invoke-WebRequest -Uri %RAGEL_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\ragel.exe"
+powershell "Invoke-WebRequest -Uri %RAGEL_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\ragel.exe"
 
 echo set (RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
 
@@ -28,7 +28,7 @@ echo set (RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
 :: Lua
 
 mkdir %DOWNLOAD_DIR%\lua
-powerhell "Invoke-WebRequest -Uri %LUA_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\lua\lua.zip"
+powershell "Invoke-WebRequest -Uri %LUA_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\lua\lua.zip"
 7z x -y %DOWNLOAD_DIR%\lua\lua.zip -o%DOWNLOAD_DIR%\lua
 
 echo set (LUA_INC_DIR %DOWNLOAD_DIR_CMAKE%/lua/include) >> paths.cmake
