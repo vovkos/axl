7772c330a5ba89c5a538122df9e9a5ade8c46ff7
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2021-07-14 16:06:22 +0800

    [ci] download lua, fix openssl paths

--- ci/github-actions/install-windows.bat
+++ ci/github-actions/install-windows.bat
@@ -29,17 +29,15 @@ echo set (RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
 
 :: Lua (download from SourceForge, need PowerShell user agent to handle redirect)
 
-echo Installing Lua...
+echo Downloading Lua...
 
-choco install lua53 --no-progress
+mkdir %DOWNLOAD_DIR%\lua
+powershell "Invoke-WebRequest -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -Uri %LUA_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\lua\lua.zip"
+7z x -y %DOWNLOAD_DIR%\lua\lua.zip -o%DOWNLOAD_DIR%\lua
 
-set LUA_ROOT_DIR=C:/ProgramData/Chocolatey/lib/lua53/tools
-
-dir C:\ProgramData\Chocolatey\lib\lua53\tools
-
-echo set (LUA_INC_DIR %LUA_ROOT_DIR%/include) >> paths.cmake
-echo set (LUA_LIB_DIR %LUA_ROOT_DIR%) >> paths.cmake
-echo set (LUA_DLL_DIR %LUA_ROOT_DIR%) >> paths.cmake
+echo set (LUA_INC_DIR %DOWNLOAD_DIR_CMAKE%/lua/include) >> paths.cmake
+echo set (LUA_LIB_DIR %DOWNLOAD_DIR_CMAKE%/lua) >> paths.cmake
+echo set (LUA_DLL_DIR %DOWNLOAD_DIR_CMAKE%/lua) >> paths.cmake
 echo set (LUA_LIB_NAME %LUA_LIB_NAME%) >> paths.cmake
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
@@ -52,9 +50,9 @@ choco install openssl --no-progress
 
 set OPENSSL_ROOT_DIR=C:/Program Files/OpenSSL-%OPENSSL_PLATFORM%
 
-echo set (OPENSSL_INC_DIR %OPENSSL_ROOT_DIR%/include) >> paths.cmake
-echo set (OPENSSL_LIB_DIR %OPENSSL_ROOT_DIR%/lib) >> paths.cmake
-echo set (OPENSSL_DLL_DIR %OPENSSL_ROOT_DIR%/bin) >> paths.cmake
+echo set (OPENSSL_INC_DIR "%OPENSSL_ROOT_DIR%/include") >> paths.cmake
+echo set (OPENSSL_LIB_DIR "%OPENSSL_ROOT_DIR%/lib") >> paths.cmake
+echo set (OPENSSL_DLL_DIR "%OPENSSL_ROOT_DIR%/bin") >> paths.cmake
 echo set (OPENSSL_CRYPTO_LIB_NAME libcrypto) >> paths.cmake
 echo set (OPENSSL_CRYPTO_DLL_NAME libcrypto%OPENSSL_DLL_SUFFIX%) >> paths.cmake
 echo set (OPENSSL_SSL_LIB_NAME libssl) >> paths.cmake
