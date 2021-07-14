b6aa0a21adca4327999504d4f4a40e128f02f33f
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2021-07-14 22:28:20 +0800

    [ci] add github ci (github actions)

---
+++ ci/github-actions/install-osx.sh
@@ -0,0 +1,20 @@
+#...............................................................................
+#
+#  This file is part of the AXL library.
+#
+#  AXL is distributed under the MIT license.
+#  For details see accompanying license.txt file,
+#  the public copy of which is also available at:
+#  http://tibbo.com/downloads/archive/axl/license.txt
+#
+#...............................................................................
+
+brew update
+brew install lua
+brew install libusb
+brew install ragel
+
+# openssl is already installed, but not linked
+
+echo "set (OPENSSL_INC_DIR /usr/local/opt/openssl/include)" >> paths.cmake
+echo "set (OPENSSL_LIB_DIR /usr/local/opt/openssl/lib)" >> paths.cmake
