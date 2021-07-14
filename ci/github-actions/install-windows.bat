2e0fc7809680d4b806d96ebd54e435709c00960a
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2021-07-14 13:12:46 +0800

    [ci] try to build with lua & openssl

--- ci/github-actions/install-windows.bat
+++ ci/github-actions/install-windows.bat
@@ -33,16 +33,14 @@ echo Installing Lua...
 
 choco install lua53 --no-progress --verbose
 
-:: echo Downloading Lua...
-::
-:: mkdir %DOWNLOAD_DIR%\lua
-:: powershell "Invoke-WebRequest -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -Uri %LUA_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\lua\lua.zip"
-:: 7z x -y %DOWNLOAD_DIR%\lua\lua.zip -o%DOWNLOAD_DIR%\lua
-::
-:: echo set (LUA_INC_DIR %DOWNLOAD_DIR_CMAKE%/lua/include) >> paths.cmake
-:: echo set (LUA_LIB_DIR %DOWNLOAD_DIR_CMAKE%/lua) >> paths.cmake
-:: echo set (LUA_DLL_DIR %DOWNLOAD_DIR_CMAKE%/lua) >> paths.cmake
-:: echo set (LUA_LIB_NAME %LUA_LIB_NAME%) >> paths.cmake
+set LUA_ROOT_DIR C:\ProgramData\Chocolatey\lib\lua53\tools
+
+dir %LUA_ROOT_DIR%
+
+echo set (LUA_INC_DIR %LUA_ROOT_DIR%/include) >> paths.cmake
+echo set (LUA_LIB_DIR %LUA_ROOT_DIR%) >> paths.cmake
+echo set (LUA_DLL_DIR %LUA_ROOT_DIR%) >> paths.cmake
+echo set (LUA_LIB_NAME %LUA_LIB_NAME%) >> paths.cmake
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
@@ -50,38 +48,16 @@ choco install lua53 --no-progress --verbose
 
 echo Installing OpenSSL...
 
-choco install openssl --no-progress --verbose
+choco install openssl --no-progress
 
-:: %DOWNLOAD_DIR%\openssl-install-log.txt
-:: echo Downloading OpenSSL...
-::
-:: powershell "Invoke-WebRequest -Uri %OPENSSL_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\openssl.msi"
-::
-:: echo Installing OpenSSL...
-::
-:: set OPENSSL_DIR=C:\Program Files\OpenSSL-%OPENSSL_PLATFORM%
-::
-:: start /wait msiexec /i %DOWNLOAD_DIR%\openssl.msi INSTALLDIR=%OPENSSL_DIR% TARGETDIR=%OPENSSL_DIR% /qn /L*V %DOWNLOAD_DIR%\openssl-install-log.txt
-::
-:: type %DOWNLOAD_DIR%\openssl-install-log.txt
-::
-:: dir %OPENSSL_DIR%
-::
-:: echo set (OPENSSL_INC_DIR %OPENSSL_ROOT_DIR%/%OPENSSL_PLATFORM%/include) >> paths.cmake
-:: echo set (OPENSSL_LIB_DIR %OPENSSL_ROOT_DIR%/%OPENSSL_PLATFORM%/lib) >> paths.cmake
-:: echo set (OPENSSL_DLL_DIR %OPENSSL_ROOT_DIR%/%OPENSSL_PLATFORM%/bin) >> paths.cmake
-:: echo set (OPENSSL_CRYPTO_LIB_NAME libcrypto) >> paths.cmake
-:: echo set (OPENSSL_CRYPTO_DLL_NAME libcrypto%OPENSSL_DLL_SUFFIX%) >> paths.cmake
-:: echo set (OPENSSL_SSL_LIB_NAME libssl) >> paths.cmake
-:: echo set (OPENSSL_SSL_DLL_NAME libssl%OPENSSL_DLL_SUFFIX%) >> paths.cmake
+set OPENSSL_ROOT_DIR "C:\Program Files\OpenSSL-%OPENSSL_PLATFORM%"
+
+echo set (OPENSSL_INC_DIR %OPENSSL_ROOT_DIR%/include) >> paths.cmake
+echo set (OPENSSL_LIB_DIR %OPENSSL_ROOT_DIR%/lib) >> paths.cmake
+echo set (OPENSSL_DLL_DIR %OPENSSL_ROOT_DIR%/bin) >> paths.cmake
+echo set (OPENSSL_CRYPTO_LIB_NAME libcrypto) >> paths.cmake
+echo set (OPENSSL_CRYPTO_DLL_NAME libcrypto%OPENSSL_DLL_SUFFIX%) >> paths.cmake
+echo set (OPENSSL_SSL_LIB_NAME libssl) >> paths.cmake
+echo set (OPENSSL_SSL_DLL_NAME libssl%OPENSSL_DLL_SUFFIX%) >> paths.cmake
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-
-dir "C:\"
-dir "C:\Program Files"
-dir "C:\Program Files (x86)"
-
-dir "C:\Program Files\OpenSSL"
-dir "C:\Program Files\OpenSSL-Win64"
-
-dir C:\ProgramData\Chocolatey\tools
