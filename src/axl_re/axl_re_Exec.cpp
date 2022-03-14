46bf738ce7aa901d131cefa27c6418fb597327e9
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2022-03-14 11:23:21 +0800

    [axl_re] initialize ExecEngine::m_prevChar

--- src/axl_re/axl_re_Exec.cpp
+++ src/axl_re/axl_re_Exec.cpp
@@ -28,6 +28,7 @@ ExecEngine::ExecEngine(
 	m_lastExecOffset = 0;
 	m_lastExecEndOffset = 0;
 	m_offset = 0;
+	m_prevChar = 0;
 	m_prevCharFlags = 0;
 	m_decoderState = 0;
 	m_execFlags = parent->m_init.m_execFlags; // cache to save one indirection
@@ -38,6 +39,7 @@ void
 ExecEngine::initialize(const StateInit& init) {
 	m_offset = init.m_offset;
 	m_decoderState = init.m_decoderState;
+	m_prevChar = 0;
 	m_prevCharFlags = init.m_prevCharFlags;
 	m_execResult = ExecResult_Continue;
 }
