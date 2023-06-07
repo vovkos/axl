bdd1cc9c6b5be111632bbf02c145b5145b84c98f
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2023-06-07 16:46:08 +0800

    [axl_io] fix: add specialization to err::complete

--- src/axl_io/axl_io_FilePathUtils.cpp
+++ src/axl_io/axl_io_FilePathUtils.cpp
@@ -327,7 +327,7 @@ getFullFilePath(const sl::StringRef& fileName) {
 #elif (_AXL_OS_POSIX)
 	char fullPath[PATH_MAX] = { 0 };
 	char* p = ::realpath(fileName.sz(), fullPath);
-	return err::complete(p, NULL);
+	return err::complete<char*>(p, NULL);
 #endif
 }
 
