3bbdef9c5c96ea20f67b86424e26e1ac86835fac
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2023-08-25 16:06:16 +0800

    [axl_sl] redesign API for uleb128/sleb128; add uleb128 encoder

--- include/axl_enc/axl_enc_Leb128.h
+++ include/axl_enc/axl_enc_Leb128.h
@@ -13,26 +13,91 @@
 
 #define _AXL_ENC_LEB128_H
 
-#include "axl_g_Pch.h"
+#include "axl_sl_UnsignedType.h"
 
 namespace axl {
 namespace enc {
 
 //..............................................................................
 
-size_t
-uleb128(
-	const void* p,
-	size_t size,
-	uint64_t* result = NULL
-);
+template <typename T>
+char*
+encodeUleb128(
+	char* p,
+	T value0
+) {
+	typedef sl::UnsignedType<T>::Type U;
 
-size_t
-sleb128(
-	const void* p,
-	size_t size,
-	int64_t* result = NULL
-);
+	U value = value0; // should be an implicit lossless conversion
+
+	for (;;) {
+		uint8_t byte = value & 0x7f;
+		value >>= 7;
+		if (!value) {
+			*p++ = byte;
+			break;
+		}
+
+		byte |= 0x80;
+		*p++ = byte;
+	};
+
+	return p;
+}
+
+template <typename T>
+const char*
+decodeUleb128(
+	T* result,
+	const char* p,
+	const char* end
+) {
+	typedef sl::UnsignedType<T>::Type U;
+
+	U value = 0;
+	uint_t shift = 0;
+
+	while (p < end) {
+		uint8_t byte = *p++;
+		value |= (U)(byte & 0x7f) << shift;
+		if (!(byte & 0x80))
+			break;
+
+		shift += 7;
+	}
+
+	*result = (T)value;
+	return p;
+}
+
+template <typename T>
+const char*
+decodeSleb128(
+	T* result,
+	const char* p,
+	const char* end
+) {
+	typedef sl::UnsignedType<T>::Type U;
+
+	U value = 0;
+	uint_t shift = 0;
+
+	while (p < end) {
+		uint8_t byte = *p++;
+		value |= (U)(byte & 0x7f) << shift;
+		shift += 7;
+
+		if (!(byte & 0x80)) {
+			if (shift < 64 && (byte & 0x40))
+				value |= ((U)-1 << shift);
+
+			break;
+		}
+	}
+
+	*result = (T)value;
+	return p;
+}
 
 //..............................................................................
 
---
+++ include/axl_sl/axl_sl_UnsignedType.h
@@ -0,0 +1,62 @@
+//..............................................................................
+//
+//  This file is part of the AXL library.
+//
+//  AXL is distributed under the MIT license.
+//  For details see accompanying license.txt file,
+//  the public copy of which is also available at:
+//  http://tibbo.com/downloads/archive/axl/license.txt
+//
+//..............................................................................
+
+#pragma once
+
+#define _AXL_SL_UNSIGNEDTYPE_H
+
+#include "axl_g_Pch.h"
+
+namespace axl {
+namespace sl {
+
+//..............................................................................
+
+template <size_t T>
+class UnsignedTypeImpl {
+};
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+template <>
+class UnsignedTypeImpl<1> {
+public:
+	typedef uint8_t Type;
+};
+
+template <>
+class UnsignedTypeImpl<2> {
+public:
+	typedef uint16_t Type;
+};
+
+template <>
+class UnsignedTypeImpl<4> {
+public:
+	typedef uint32_t Type;
+};
+
+template <>
+class UnsignedTypeImpl<8> {
+public:
+	typedef uint64_t Type;
+};
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+template <typename T>
+class UnsignedType: public UnsignedTypeImpl<sizeof(T)> {
+};
+
+//..............................................................................
+
+} // namespace sl
+} // namespace axl
