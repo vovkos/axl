b6ab16c736306845e1f8af189863c8b505a30255
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2023-09-13 15:17:53 +0800

    [axl_io_hid] remove unnecessary includes from pch.h

--- src/axl_io_hid/pch.h
+++ src/axl_io_hid/pch.h
@@ -13,18 +13,3 @@
 
 #include "axl_io_HidPch.h"
 #include "axl_enc_Leb128.h"
-
-#if (_AXL_OS_WIN)
-#	include "axl_sys_win_DeviceInfo.h"
-#	include "axl_io_win_File.h"
-
-#	include <cfgmgr32.h>
-#	include <winioctl.h>
-#	include <usbioctl.h>
-#	include <usb.h>
-#elif (_AXL_OS_LINUX)
-#	include "axl_sys_lnx_UdevHwdb.h"
-#elif (_AXL_OS_DARWIN)
-#	include "axl_iok_RegistryEntry.h"
-#	include <IOKit/usb/IOUSBLib.h>
-#endif
