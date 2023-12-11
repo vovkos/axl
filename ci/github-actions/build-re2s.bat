767f81fab49fd402eb98e23074ac6eee06c2aa0c
Vladimir Gladkov (office-arch) <vovkos@gmail.com>
2023-12-11 09:49:14 +0800

    [ci] move re2s build to dedicated files

---
+++ ci/github-actions/build-re2s.bat
@@ -0,0 +1,32 @@
+::..............................................................................
+::
+::  This file is part of the AXL library.
+::
+::  AXL is distributed under the MIT license.
+::  For details see accompanying license.txt file,
+::  the public copy of which is also available at:
+::  http://tibbo.com/downloads/archive/axl/license.txt
+::
+::..............................................................................
+
+@echo off
+
+:: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+mkdir re2s\.build
+cd re2s\.build
+
+cmake .. ^
+	-DCMAKE_INSTALL_PREFIX=%THIS_DIR_CMAKE%/re2s/install ^
+	-DBUILD_SHARED_LIBS=OFF ^
+	-DRE2_BUILD_TESTING=OFF ^
+	|| exit
+
+cmake --build . --target install %CMAKE_BUILD_FLAGS% || exit
+
+cd %THIS_DIR%
+echo set(RE2S_CMAKE_DIR %THIS_DIR_CMAKE%/re2s/install/lib/cmake/re2s) >> paths.cmake
+
+type paths.cmake
+
+:: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
---
+++ ci/github-actions/build-re2s.sh
@@ -0,0 +1,31 @@
+#...............................................................................
+#
+#  This file is part of the AXL library.
+#
+#  AXL is distributed under the MIT license.
+#  For details see accompanying license.txt file,
+#  the public copy of which is also available at:
+#  http://tibbo.com/downloads/archive/axl/license.txt
+#
+#...............................................................................
+
+THIS_DIR=$(pwd)
+
+mkdir re2s/.build
+pushd re2s/.build
+
+echo config: $1
+
+cmake .. \
+	-DCMAKE_INSTALL_PREFIX=$THIS_DIR/re2s/install \
+	-DCMAKE_BUILD_TYPE=$1 \
+	-DCMAKE_CXX_FLAGS=-fPIC \
+	-DBUILD_SHARED_LIBS=OFF \
+	-DRE2_BUILD_TESTING=OFF
+
+make install
+popd
+
+echo set(RE2S_CMAKE_DIR $THIS_DIR/re2s/install/lib/cmake/re2s) >> paths.cmake
+
+cat paths.cmake
