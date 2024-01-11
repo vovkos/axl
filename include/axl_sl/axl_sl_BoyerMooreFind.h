595d5afee8d5d22bca1baad11e2bf6bd7d63fa9d
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2024-01-11 15:16:49 +0800

    [axl_sl] fix: use the bin-tail decoder state in locateBinOffset

--- include/axl_sl/axl_sl_BoyerMooreFind.h
+++ include/axl_sl/axl_sl_BoyerMooreFind.h
@@ -457,7 +457,6 @@ protected:
 			else { // two disjoint chunks
 				p = state->getBinTailBuffer();
 				end = state->getBinTailBufferEnd();
-				enc::DecoderState decoderState = 0;
 
 				if (IsReverse) {
 					result = Locate::locate(&decoderState, i, back - 1, p - 1); // back first
