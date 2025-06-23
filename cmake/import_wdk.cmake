c1839a746df37caf3f0543ce26b611ba5e6eda51
Vladimir Gladkov (vladimir-office-win11) <vovkos@gmail.com>
2025-06-23 16:19:30 +0800

    [cmake] make use of FindWDK.cmake when searching for WDK dirs

--- cmake/import_wdk.cmake
+++ cmake/import_wdk.cmake
@@ -9,12 +9,37 @@
 #
 #...............................................................................
 
-if(EXISTS ${WDK_DDK_INC_DIR}/ntddk.h)
+if(FIND_WDK_CMAKE_DIR)
+	list(
+		APPEND
+		CMAKE_MODULE_PATH
+		${FIND_WDK_CMAKE_DIR}
+	)
+
+	if(WDK_ROOT_DIR)
+		set(ENV{WDKContentRoot} "${WDK_ROOT_DIR}")
+	endif()
+
+	find_package(WDK QUIET)
+
+	if(WDK_FOUND)
+		set(WDK_SDK_INC_DIR "${WDK_ROOT}/Include/${WDK_INC_VERSION}/shared")
+		set(WDK_CRT_INC_DIR "${WDK_ROOT}/Include/${WDK_INC_VERSION}/km/crt")
+		set(WDK_DDK_INC_DIR "${WDK_ROOT}/Include/${WDK_INC_VERSION}/km")
+		set(WDK_DDK_LIB_DIR "${WDK_ROOT}/Lib/${WDK_LIB_VERSION}/km/${WDK_PLATFORM}")
+
+		axl_message("WDK paths:")
+		axl_message("    SDK includes:"  "${WDK_SDK_INC_DIR}")
+		axl_message("    CRT includes:"  "${WDK_CRT_INC_DIR}")
+		axl_message("    DDK includes:"  "${WDK_DDK_INC_DIR}")
+		axl_message("    DDK libraries:" "${WDK_DDK_LIB_DIR}")
+	endif()
+elseif(EXISTS ${WDK_DDK_INC_DIR}/ntddk.h)
 	axl_message("WDK paths:")
 	axl_message("    WDK root:"      "${WDK_ROOT_DIR}")
 	axl_message("    WDK binaries:"  "${WDK_BIN_DIR}")
-	axl_message("    CRT includes:"  "${WDK_CRT_INC_DIR}")
 	axl_message("    SDK includes:"  "${WDK_SDK_INC_DIR}")
+	axl_message("    CRT includes:"  "${WDK_CRT_INC_DIR}")
 	axl_message("    DDK includes:"  "${WDK_DDK_INC_DIR}")
 	axl_message("    CRT libraries:" "${WDK_CRT_LIB_DIR}")
 	axl_message("    DDK libraries:" "${WDK_DDK_LIB_DIR}")
