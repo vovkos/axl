9af183347cfb73894ce2b729fa99075a9f6e19ce
Vladimir Gladkov (vladimir-office-win11) <vovkos@gmail.com>
2026-02-03 13:30:17 +0800

    [axl_py] add more error utils (raiseException/failCFunction/completeCFunction)

--- include/axl_py/axl_py_Error.h
+++ include/axl_py/axl_py_Error.h
@@ -94,14 +94,26 @@ raiseException(
 
 inline
 void
+raiseException(const char* description) {
+	::PyErr_SetString(PyExc_Exception, description);
+}
+
+inline
+void
 raiseException(
 	PyObject* exception,
 	const sl::StringRef& description
 ) {
-	::PyErr_SetString(exception, description.sz());
+	raiseException(exception, description.sz());
 }
 
-// later add a dedicated error object that would get description on demand
+inline
+void
+raiseException(const sl::StringRef& description) {
+	raiseException(description.sz());
+}
+
+// later maybe add a dedicated error object that would get description on demand
 
 inline
 void
@@ -109,7 +121,74 @@ raiseException(
 	PyObject* exception,
 	const err::ErrorRef& error = err::getLastError()
 ) {
-	::PyErr_SetString(exception, error.getDescription().sz());
+	raiseException(exception, error.getDescription().sz());
+}
+
+inline
+void
+raiseException(const err::ErrorRef& error = err::getLastError()) {
+	raiseException(error.getDescription().sz());
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+inline
+PyObject*
+failCFunction(
+	PyObject* exception,
+	const char* description
+) {
+	raiseException(exception, description);
+	return NULL;
+}
+
+inline
+PyObject*
+failCFunction(const char* description) {
+	raiseException(description);
+	return NULL;
+}
+
+inline
+PyObject*
+failCFunction(
+	PyObject* exception,
+	const sl::StringRef& description
+) {
+	raiseException(exception, description);
+	return NULL;
+}
+
+inline
+PyObject*
+failCFunction(const sl::StringRef& description) {
+	raiseException(description);
+	return NULL;
+}
+
+inline
+PyObject*
+failCFunction(
+	PyObject* exception,
+	const err::ErrorRef& error = err::getLastError()
+) {
+	raiseException(exception, error);
+	return NULL;
+}
+
+inline
+PyObject*
+failCFunction(const err::ErrorRef& error = err::getLastError()) {
+	raiseException(error);
+	return NULL;
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+inline
+PyObject*
+completeCFunction(PyObject* result) {
+	return result ? result : failCFunction();
 }
 
 //..............................................................................
