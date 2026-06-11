4a36899ff1aac9469c64470ae1c5e8c87e1a3cde
Vladimir Gladkov (office-ryzen-arch) <vovkos@gmail.com>
2026-06-11 11:42:48 +0800

    [axl_sl] c++11 impl for ArgType<T>

--- include/axl_sl/axl_sl_ArgType.h
+++ include/axl_sl/axl_sl_ArgType.h
@@ -20,82 +20,10 @@ namespace sl {
 
 //..............................................................................
 
-// for most aggregates, we want to pass it as a const-reference
-
 template <typename T>
 class ArgType {
 public:
-	typedef const T& Type;
-};
-
-// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-
-// for simple types, its more efficient to pass it as is
-
-template <typename T>
-class SimpleArgType {
-public:
-	typedef T Type;
-};
-
-// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-
-// specialization for primitive types
-
-template <>
-class ArgType<char>: public SimpleArgType<char> {
-};
-
-template <>
-class ArgType<wchar_t>: public SimpleArgType<wchar_t> {
-};
-
-template <>
-class ArgType<float>: public SimpleArgType<float> {
-};
-
-template <>
-class ArgType<double>: public SimpleArgType<double> {
-};
-
-template <>
-class ArgType<int8_t>: public SimpleArgType<int8_t> {
-};
-
-template <>
-class ArgType<uint8_t>: public SimpleArgType<uint8_t> {
-};
-
-template <>
-class ArgType<int16_t>: public SimpleArgType<int16_t> {
-};
-
-template <>
-class ArgType<uint16_t>: public SimpleArgType<uint16_t> {
-};
-
-template <>
-class ArgType<int32_t>: public SimpleArgType<int32_t> {
-};
-
-template <>
-class ArgType<uint32_t>: public SimpleArgType<uint32_t> {
-};
-
-template <>
-class ArgType<int64_t>: public SimpleArgType<int64_t> {
-};
-
-template <>
-class ArgType<uint64_t>: public SimpleArgType<uint64_t> {
-};
-
-// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-
-// specialization for pointers
-
-template <typename T>
-class ArgType<T*>: public SimpleArgType<T*> {
+	typedef typename std::conditional<std::is_scalar<T>::value, T, const T&>::type Type;
 };
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
