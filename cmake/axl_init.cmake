0c152d8cc1ff91e24f58ac2bcb311b53fd3063d0
Vladimir Gladkov (office-mac-mini-m1) <vovkos@gmail.com>
2026-06-18 10:20:47 +0800

    [cmake] unless configured explicitly, use c++11

--- cmake/axl_init.cmake
+++ cmake/axl_init.cmake
@@ -64,6 +64,10 @@ endif()
 axl_apply_std_settings()
 axl_print_std_settings()
 
+if(NOT "${GCC_FLAG_CPP_STANDARD}" MATCHES "-std=")
+	set(CMAKE_CXX_STANDARD 11) # unless specified explicitly, use c++11
+endif()
+
 # re-include paths.cmake and dependencies.cmake -- this time ${CONFIGURATION}
 # and all the other settings are defined
 
