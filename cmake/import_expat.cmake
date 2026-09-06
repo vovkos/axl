a8df67466dce23f55274ca6cfe3e6915f545ad2d
Vladimir Gladkov (laptop2-arch) <vovkos@gmail.com>
2026-09-06 12:06:39 +0800

    [cmake] expat: add alternative lib names (to support newer expat versions)

--- cmake/import_expat.cmake
+++ cmake/import_expat.cmake
@@ -9,15 +9,38 @@
 #
 #...............................................................................
 
+set(
+	_EXPAT_LIB_NAME_LIST
+	expat
+	libexpat
+)
+
 if(NOT EXPAT_INC_DIR)
 	axl_find_inc_dir(EXPAT_INC_DIR expat.h)
 endif()
 
 if(NOT EXPAT_LIB_DIR)
-	axl_find_lib_dir(EXPAT_LIB_DIR expat)
+	if(NOT EXPAT_LIB_NAME)
+		axl_find_lib_dir_ex(
+			RESULT_LIB_DIR EXPAT_LIB_DIR
+			RESULT_LIB_NAME EXPAT_LIB_NAME
+			LIB_NAME ${_EXPAT_LIB_NAME_LIST}
+		)
+	else()
+		axl_find_lib_dir_ex(
+			RESULT_LIB_DIR EXPAT_LIB_DIR
+			LIB_NAME ${EXPAT_LIB_NAME}
+		)
+	endif()
+elseif(NOT EXPAT_LIB_NAME)
+	axl_find_lib_dir_ex(
+		RESULT_LIB_NAME EXPAT_LIB_NAME
+		LIB_DIR ${EXPAT_LIB_DIR}
+		LIB_NAME ${_EXPAT_LIB_NAME_LIST}
+	)
 endif()
 
-if(EXISTS ${EXPAT_INC_DIR}/expat.h AND EXPAT_LIB_DIR)
+if(EXISTS ${EXPAT_INC_DIR}/expat.h AND EXPAT_LIB_DIR AND EXPAT_LIB_NAME)
 	axl_message("Expat paths:")
 	axl_message("    Includes:"  "${EXPAT_INC_DIR}")
 	axl_message("    Libraries:" "${EXPAT_LIB_DIR}")
@@ -25,6 +48,8 @@ if(EXISTS ${EXPAT_INC_DIR}/expat.h AND EXPAT_LIB_DIR)
 	if(EXPAT_DLL_DIR)
 		axl_message("    DLLs:"  "${EXPAT_DLL_DIR}")
 	endif()
+
+	axl_message("    Library name:" "${EXPAT_LIB_NAME}")
 
 	set(EXPAT_FOUND TRUE)
 else()
